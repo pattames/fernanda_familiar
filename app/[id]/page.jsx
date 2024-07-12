e73e2672dca922a1682603415dc7f3cd626a809b
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-07-12 15:00:20 -0600

    vista detalle terminada

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -2,11 +2,18 @@ import Image from "next/image";
 import parse from "html-react-parser";
 
 export default async function Noticia({ params }) {
+  //Fetch posts
   const res = await fetch("https://fernandafamiliar.soy/wp-json/wp/v2/posts");
   const posts = await res.json();
 
   //Encontrar noticia seleccionada
   const post = posts.find((post) => post.id === Number(params.id));
+
+  //Fetch author
+  const res2 = await fetch(
+    `https://fernandafamiliar.soy/wp-json/wp/v2/users/${post.author}`
+  );
+  const author = await res2.json();
 
   return (
     <div className="max-w-7xl flex flex-col mx-auto py-10 px-6 gap-8 border-2 fade-in">
@@ -23,7 +30,9 @@ export default async function Noticia({ params }) {
         />
       )}
       <div className="text-center">
-        <h2>Por {} </h2>
+        <h2>
+          Por <span className="font-bold">{author.name && author.name}</span>
+        </h2>
         <h2>{post.date && post.date}</h2>
       </div>
       {post.content.rendered && (
