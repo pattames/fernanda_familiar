4aa0d65464363c8e158557312a7119a55cc8e7a4
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-07-15 12:49:04 -0600

    loading and error page sin estilos

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,14 @@
 import Tarjeta from "./components/Tarjeta";
 
 export default async function Noticias() {
-  try {
-    const res = await fetch(
-      "https://fernandafamiliar.soy/wp-json/wp/v2/posts",
-      {
-        cache: "no-store",
-      }
-    );
+  const res = await fetch("https://fernandafamiliar.soy/wp-json/wp/v2/posts", {
+    cache: "no-store",
+  });
+  const posts = await res.json();
 
-    if (!res.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const posts = await res.json();
-
-    return (
-      <div className="p-4 flex flex-wrap gap-4 justify-center fade-in">
-        {posts && posts.map((post) => <Tarjeta post={post} key={post.id} />)}
-      </div>
-    );
-  } catch (error) {
-    console.error("Failed to fetch posts:", error);
-    return <div>Error loading posts. Please try again later.</div>;
-  }
+  return (
+    <div className="p-4 flex flex-wrap gap-4 justify-center fade-in">
+      {posts && posts.map((post) => <Tarjeta post={post} key={post.id} />)}
+    </div>
+  );
 }
