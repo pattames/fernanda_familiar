c3f23ed249df5e2b8d54c47daa85c41cba1a9c19
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-08-05 17:08:19 -0600

    mover icons a app route y agregar link tags a html

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <Banner />
         {children}
